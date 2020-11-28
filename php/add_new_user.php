2bc7b9ccbf791db9e464432ed9cccebb594b7da5
John Michael Hayde <johnmichaelhayde@gmail.com>
2020-11-27 19:26:08 -0500

    added mysql connect for production database

--- php/add_new_user.php
+++ php/add_new_user.php
@@ -7,6 +7,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
       $_POST["password"] == $_POST["password_confir"]) {
       // Since user info approved, add them to user table
 			$conn = mysqli_connect("127.0.0.1", "root", "johnmichael06", "4620_proj");
+			//$conn = mysqli_connect("mysql1.cs.clemson.edu", "cpsc4620_1_1ncu", "johnmichael06", "cpsc4620_1_7ma4");
 			// check connection
 			if ($conn->connect_error) {
 				die("Connection failed: " . $conn->connect_error);
