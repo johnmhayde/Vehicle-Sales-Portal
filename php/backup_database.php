eedf8e00ee68704e929cfe698d477895ed7cb828
John Michael Hayde <johnmichaelhayde@gmail.com>
2021-10-28 13:41:29 -0400

    cleaning up code

--- php/backup_database.php
+++ php/backup_database.php
@@ -1,7 +1,6 @@
 <?php
 // Connect to database
-$conn = mysqli_connect("mysql1.cs.clemson.edu", "cpsc4620_1_1ncu", "johnmichael06", "cpsc4620_1_7ma4");
-//$conn = mysqli_connect("127.0.0.1", "root", "johnmichael06", "4620_proj");
+$conn = mysqli_connect("127.0.0.1", "root", "password", "db_name");
 // check connection
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
